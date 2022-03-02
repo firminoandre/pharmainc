3009e66a712b68d55d7cfd308c56d41029c28084
Firmino Andre <andrezfirmino@gmail.com>
2022-03-02 12:39:45 -0300

    initial config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
+import { createWebHistory, createRouter,  } from "vue-router";
 import App from './App.vue'
+import './assets/tailwind.css'
+import Dashboard from './components/Dashboard'
 
-createApp(App).mount('#app')
+
+const routes = [
+    {
+      name: "Dashboard",
+      path: "/",
+      component: Dashboard,
+    },
+  ];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+
+createApp(App).use(router).mount('#app')
