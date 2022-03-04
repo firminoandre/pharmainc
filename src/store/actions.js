34d58295aa75d13d62c4a660dca2a8b621053c10
Firmino Andre <andrezfirmino@gmail.com>
2022-03-04 09:38:02 -0300

    fix

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
 
-export const getPacientes = ({ commit }) => {
-    axios.get('https://randomuser.me/api/', { params: { results: 50 } })
+export const getPacientes = ({ commit }, gender) => {
+    axios.get('https://randomuser.me/api/', { 
+        params: { 
+            results: 50,
+            gender: gender
+        } 
+
+    })
     .then(response => {
         commit('GET_PACIENTES', response.data.results)
     })
 }
+
